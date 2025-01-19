d49b7108b08a1a25544fbeb7f0c858345b12db3e
Alejandro <deolozaga2004@gmail.com>
2025-01-19 17:34:57 +0000

    Main finished.

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import fixers.FixerCurrencyLoader;
+import model.Currency;
+import model.CurrencyLoader;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        CurrencyLoader currencyLoader = new FixerCurrencyLoader();
+        List<Currency> currencies = currencyLoader.load();
+        for (Currency currency : currencies) {
+            System.out.println(currency);
+        }
+    }
+}
